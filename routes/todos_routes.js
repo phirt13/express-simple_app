bb45219d799ccf850d2b18628d4524f997d143ed
Philip A. Hirt <phirt13@gmail.com>
2015-07-09 04:09:27 -0700

    built RESTful core functionality

--- routes/todos_routes.js
+++ routes/todos_routes.js
@@ -1,16 +1,20 @@
 'use strict';
 
-var Todo = require('../models/Todo');
+var Todo = require('../models/Todo.js');
 var bodyParser = require('body-parser');
 
 module.exports = function(router) {
+
   router.use(bodyParser.json());
 
   router.post('/create_todos', function(req, res) {
+
     console.log('Inside create todos!');
     console.log(req.body);
+
     var newTodoData = JSON.parse(JSON.stringify(req.body));
     var newTodo = new Todo(newTodoData);
+
     newTodo.save(function(err, newTodo) {
       if(err) {
         console.log(err);
@@ -24,14 +28,16 @@ module.exports = function(router) {
   });
 
   router.get('/get_todos', function(req, res) {
+
     console.log('Inside get todos!');
+
     Todo.find({}, function(err, data) {
       if(err) {
         console.log(err);
         return res.status(500).json({ msg : 'internal server error'});
       } else {
         console.log('Mongo says: "Got it!"');
-        conosle.log(data);
+        console.log(data);
 
         res.json(data);
       }
@@ -39,7 +45,9 @@ module.exports = function(router) {
   });
 
   router.put('/update_todos', function(req, res) {
+
     console.log('Inside update todos!');
+
     Todo.update({ _id : req.body._id }, { todo : req.body.todo }, function(err, result) {
       if(err) {
         console.log(err);
@@ -63,8 +71,10 @@ module.exports = function(router) {
   });
 
   router.delete('/remove_todos', function(req, res) {
+
     console.log('Inside remove todo!');
     console.log(req.body);
+
     Todo.remove({ _id : req.body._id }, function(err, result) {
       if(err) {
         console.log(err);
